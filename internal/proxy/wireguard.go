f34904b61d659e55428988fe0975d37cfcce11df
salman-emb <salman.malik@emumba.com>
2022-12-19 15:33:01 +0500

    Added ping message utc time

--- internal/proxy/wireguard.go
+++ internal/proxy/wireguard.go
@@ -134,7 +134,7 @@ func (p *WireguardProxy) proxyToLocal() {
 		default:
 			n, err := p.remoteConn.Read(buf)
 
-			tReached := time.Now().UTC().Unix()
+			tReached := time.Now().UTC().UnixNano()
 			if err != nil {
 				continue
 			}
@@ -149,7 +149,7 @@ func (p *WireguardProxy) proxyToLocal() {
 				if !strings.Contains(msg, "REPLY") {
 					pingMsg.Message = fmt.Sprintf("[REPLY] of message=[%s]", pingMsg.Message)
 					pingMsg.DestReachedTime = tReached
-					pingMsg.ReplyInitiatedTime = time.Now().UTC().Unix()
+					pingMsg.ReplyInitiatedTime = time.Now().UTC().UnixNano()
 
 					reply, _ := json.Marshal(pingMsg)
 					_, err = p.remoteConn.Write(reply)
@@ -157,7 +157,7 @@ func (p *WireguardProxy) proxyToLocal() {
 						continue
 					}
 				} else {
-					pingMsg.ReplyReachedTime = time.Now().UTC().Unix()
+					pingMsg.ReplyReachedTime = time.Now().UTC().UnixNano()
 
 					//log.Debugf("Total time consumed %d", time.Parse(time.))
 				}
